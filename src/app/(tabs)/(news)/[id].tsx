d01c4898f4237ee677eebda8fb7893ded31a0193
lqhuy04 <lehanam2004@gmail.com>
2024-09-07 21:34:19 +0700

    bews with searchbar and web view

---
+++ src/app/(tabs)/(news)/[id].tsx
@@ -0,0 +1,123 @@
+// import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+// import React, { useEffect, useState } from 'react'
+// import { router, Stack, useLocalSearchParams } from 'expo-router'
+// import { Ionicons } from '@expo/vector-icons'
+// import { NewsDataType } from '@/constants/types'
+// import axios from 'axios'
+// import Loading from '@/components/(news)/Loading'
+
+// type Props = {}
+
+// const NewsDetail = (props: Props) => {
+//     const {id} = useLocalSearchParams<{id: string} > ();
+//     const [news, setNews] = useState<NewsDataType[]>([]);
+//     const [isLoading, setIsLoading] = useState(true);
+
+//     useEffect(() => {
+//         getNews();
+//     }, []);
+
+//     const getNews = async() => {
+//         try {
+//           const URL = `https://newsdata.io/api/1/news?apikey=pub_52598c53df161fd8e22d6d5e64f37f8410d13&id=${id}`
+//           const response = await axios .get(URL);
+    
+//           //console.log("News Data: ", response.data);
+//           if (response && response.data){
+//             setNews(response.data.results);
+//             setIsLoading(false);
+//           }
+//         } catch (err: any){
+//           console.log('Error Message: ', err.message);
+//         }
+//       }
+
+//   return (
+//     <>
+//     <Stack.Screen options = {{
+//         headerLeft: () => (
+//             <TouchableOpacity onPress={() => router.back()}>
+//                 <Ionicons name='arrow-back' size = {22} />
+//             </TouchableOpacity>
+//         ),
+//         headerRight: () => (
+//             <TouchableOpacity onPress={() => {}}>
+//                 <Ionicons name='heart-outline' size = {22} />
+//             </TouchableOpacity>
+//         ),
+//         title: ''
+//     }} />
+//     {isLoading ? (
+//         <Loading size = {'large'} />
+//     ): (   
+//     <View>
+//       <Text>{news[0].title} </Text>
+//       <Text>{news[0].content} </Text>
+//     </View>
+//     )}
+//     </>
+//   )
+// }
+
+// export default NewsDetail
+
+// const styles = StyleSheet.create({})
+// WebViewScreen.tsx
+// app/news/[id].tsx
+// app/news/[id].tsx
+// WebViewScreen.tsx
+import React, { useState } from 'react';
+import { WebView } from 'react-native-webview';
+import { View, ActivityIndicator, StyleSheet, Text, Button, SafeAreaView } from 'react-native';
+import { useLocalSearchParams } from 'expo-router';
+import * as Linking from 'expo-linking';
+import Constants from 'expo-constants';
+
+const WebViewScreen = () => {
+  // Use useLocalSearchParams to access the URL
+  const { url } = useLocalSearchParams<{ url: string }>();
+  const [favourite, setFavourite] = useState('');
+
+  return (
+    // <View style={styles.container}>
+    //   <Button
+    //     title="Open URL with the system browser"
+    //     onPress={() => Linking.openURL(url)}
+    //   />
+    //   <Button
+    //     title="Open URL with an in-app browser"
+    //     onPress={() => WebBrowser.openBrowserAsync('https://expo.dev')}
+    //   />
+    // </View>
+    //<SafeAreaView style = {styles.container}>
+    <View style={styles.container}>
+    <WebView
+    //style={styles.container}
+    style = {{marginTop: 0}}
+    source={{ uri: url }}
+  />
+  </View>
+  //</SafeAreaView>
+  );
+};
+
+export default WebViewScreen;
+
+const styles = StyleSheet.create({
+  loader: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: [{ translateX: -25 }, { translateY: -25 }],
+  },
+  container: {
+    flex: 1,
+    //alignItems: 'center',
+    //justifyContent: 'center',
+    padding: 0,
+    margin: 0
+  },
+  button: {
+    marginVertical: 10,
+  },
+});
---
+++ src/app/(tabs)/(news)/index.tsx
@@ -0,0 +1,87 @@
+import { View, Text, StyleSheet, ActivityIndicator, ScrollView } from 'react-native'
+import React, {useEffect, useState} from 'react'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import Header from '@/src/components/(news)/Header'
+import SearchBar from '@/src/components/(news)/SearchBar'
+import axios from 'axios'
+import { NewsDataType } from '@/constants/types'
+import BreakingNews from '@/src/components/(news)/BreakingNews'
+import Categories from '@/src/components/(news)/Categories'
+import NewsList from '@/src/components/(news)/NewsList'
+import Loading from '@/src/components/(news)/Loading'
+
+type Props = {}
+
+const index = (props: Props) => {
+  const {top: safeTop} = useSafeAreaInsets();
+  const [breakingNews, setBreakingNews] = useState<NewsDataType[]>([]);
+  const [news, setNews] = useState<NewsDataType[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  
+  useEffect(()=> {
+    getBreakingNews();
+    getNews();
+  }, []);
+
+  const getBreakingNews = async() => {
+    try {
+      const URL = 'https://newsdata.io/api/1/news?apikey=pub_52598c53df161fd8e22d6d5e64f37f8410d13&country=us&language=en&image=1&removeduplicate=1&size=5'
+      const response = await axios .get(URL);
+
+      //console.log("News Data: ", response.data);
+      if (response && response.data){
+        setBreakingNews(response.data.results);
+        setIsLoading(false);
+      }
+    } catch (err: any){
+      console.log('Error Message: ', err.message);
+    }
+  }
+  const getNews = async(category:string = '') => {
+    try {
+      let categoryString = '';
+      if (category.length !== 0){
+        categoryString = `&category=${category}`
+      }
+      const URL = `https://newsdata.io/api/1/news?apikey=pub_52598c53df161fd8e22d6d5e64f37f8410d13&country=us&language=en&image=1&removeduplicate=1&size=10${categoryString}`      
+      const response = await axios .get(URL);
+
+      //console.log("News Data: ", response.data);
+      if (response && response.data){
+        setNews(response.data.results);
+        setIsLoading(false);
+      }
+    } catch (err: any){
+      console.log('Error Message: ', err.message);
+    }
+  }
+
+  const onCatChanged = (category: string) => {
+    console.log('Category: ', category)
+    setNews([]);
+    getNews(category);
+  }
+  return (
+    <ScrollView style = {[styles.container, {paddingTop: 10}]}>
+      <Header />
+      <SearchBar />
+      {isLoading ? (
+        <Loading size = {'large'} />
+      ) : (
+      <BreakingNews newsList={breakingNews} />
+    )}
+    <Categories onCategoryChanged={onCatChanged}/>
+    <NewsList newsList={news}/>
+    </ScrollView>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create(
+  {
+    container: {
+      //flex:1,
+    },
+  }
+)
