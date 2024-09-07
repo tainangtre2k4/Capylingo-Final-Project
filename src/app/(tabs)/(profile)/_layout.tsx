58e55c139ffd63330df7f9f9efda2609a3e6fb35
nvht <nvht179@gmail.com>
2024-09-07 13:27:50 +0700

    fix cloudinary package version and update router for learn and dictionary
    fix cloudinary package version and update router for learn and dictionary

--- src/app/(tabs)/(profile)/_layout.tsx
+++ src/app/(tabs)/(profile)/_layout.tsx
@@ -1,11 +1,18 @@
 import { Stack } from "expo-router";
-import { SafeAreaView } from "react-native";
-import {StatusBar} from 'expo-status-bar';
+import { SafeAreaView, Platform } from "react-native";
+import { StatusBar } from 'expo-status-bar';
 
-export default function ProfileLayout(){
+export default function ProfileLayout() {
     return (
-    <SafeAreaView style={{flex: 1}}>
-        <Stack screenOptions={{headerShown: false}}/>
-    </SafeAreaView>
+        <SafeAreaView style={{ flex: 1 }}>
+            <Stack screenOptions={{
+                headerShown: false, ...Platform.select({
+                    android: {
+                        statusBarColor: '#3DB2FF',
+                        statusBarStyle: 'light',
+                    }
+                })
+            }} />
+        </SafeAreaView>
     )
 }
